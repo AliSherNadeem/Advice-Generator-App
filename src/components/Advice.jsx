492057be5afc2dc9b7b19d2ad042acbbde2f339a
AliSherNadeem <alisher786389@gmail.com>
2024-07-05 11:32:38 +0500

    fix design

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -4,29 +4,36 @@ import desktopDivider from "../assets/pattern-divider-desktop.svg";
 import buttonIcon from "../assets/icon-dice.svg";
 
 const Advice = () => {
+
+  
   return (
     <div className="bg-gray-800 w-full h-screen flex justify-center items-center font-signature">
-      <div className="flex flex-col justify-center items-center bg-gray-700 text-slate-300 p-6 sm:p-8 rounded-lg max-w-xs md:max-w-md lg:max-w-lg ">
-        <div className="text-emerald-300 mb-4 ">
-          <p>Advice # 117</p>
-        </div>
-        <div>
-          <p className="text-xl text-center sm:text-base md:text-2xl">
-            This is advice. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. expedita quia dolorem neque!
+      <div className="relative flex flex-col justify-center items-center gap-4 bg-gray-700 text-slate-300 p-6 sm:p-8 rounded-lg max-w-xs md:max-w-md lg:max-w-lg">
+        <div className="text-emerald-300 mb-4">
+          <p>
+            ADVICE # <span id="advice-id">117</span>
           </p>
         </div>
-        <div className="pt-4">
-          <img src={mobileDivider} alt="pattern-divider-mobile" />
+        <div>
+          <p className="text-xl text-center sm:text-base md:text-2xl mb-4">
+            This is advice. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Expedita quia dolorem neque!
+          </p>
+        </div>
+        <div className="mb-6">
+          <img
+            src={mobileDivider}
+            alt="pattern-divider-mobile"
+            className="block md:hidden"
+          />
           <img
             src={desktopDivider}
-            className="hidden"
             alt="pattern-divider-desktop"
+            className="hidden md:block"
           />
         </div>
-
-        <div className="pt-4">
-          <button>
+        <div className="absolute bottom-0 flex justify-center w-full">
+          <button className="bg-emerald-300 w-14 h-14 rounded-full flex justify-center items-center -mb-7 shadow-emerald-300 hover:shadow-2xl transition-shadow duration-200">
             <img src={buttonIcon} alt="icon-dice" />
           </button>
         </div>
